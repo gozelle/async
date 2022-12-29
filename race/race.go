a7cac05a8253dfb12ac9ad0bf48089dfbcee482d
koyeo <koyeo@qq.com>
2022-12-29 13:14:26 +0800

    add comment

--- race/race.go
+++ race/race.go
@@ -8,6 +8,9 @@ import (
 	"time"
 )
 
+// Race
+// 并发执行 Handler, 返回其中最快的结果
+// 如果全部返回错误，则返回出现的第一个错误
 func Race(ctx context.Context, handlers ...godash.Handler) (result any, err error) {
 	
 	if len(handlers) == 0 {
@@ -57,6 +60,10 @@ func Race(ctx context.Context, handlers ...godash.Handler) (result any, err erro
 	return
 }
 
+// DelayRace
+// 并发执行 Handler, 返回其中最快的结果
+// 如果全部返回错误，则返回出现的第一个错误
+// 与 Race 不同的是，配置延迟执行的 Handler，会在等待配置时间后，再开始执行
 func DelayRace(ctx context.Context, handlers ...*godash.DelayHandler) (result any, err error) {
 	
 	if len(handlers) == 0 {
