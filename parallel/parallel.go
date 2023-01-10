da19b167e31353d33407f06d937197b79e822c0c
koyeo <koyeo@qq.com>
2023-01-10 16:22:37 +0800

    add parallel

--- parallel/parallel.go
+++ parallel/parallel.go
@@ -54,8 +54,8 @@ func Run(ctx context.Context, runners ...async.Runner) (result []any, err error)
 
 // RunLimit 分片并发任务
 // 效果：将会按照 limit 定义的数值，使用回调函数对任务进行分组处理
-// 回调函数是同步运行
-// 如果回调函数返回错误，将会终止运行
+// 回调函数会同步运行,如果回调函数返回错误，将会终止 RunLimit 运行, 返回错误；
+// 如果希望在分组全部运行结束后，再提交回调函数处理后的结果，请在外面做好事务控制。
 func RunLimit(ctx context.Context, limit int, callback func(values []any) error, runners ...async.Runner) (err error) {
 	
 	if limit <= 0 {
