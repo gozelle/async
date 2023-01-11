e9f936456759631bbb920e9a66c2dbd45b4364c8
koyeo <koyeo@qq.com>
2023-01-11 18:23:22 +0800

    update retry

--- retry/retry_test.go
+++ retry/retry_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestRun(t *testing.T) {
 	now := time.Now()
-	r, err := Run(context.Background(), 2, 3*time.Second, func(ctx context.Context) (result any, err error) {
+	r, err := Run(context.Background(), 3, 10*time.Second, func(ctx context.Context) (result any, err error) {
 		t.Log(time.Now())
 		d := time.Since(now)
 		if d < 2*time.Second {
