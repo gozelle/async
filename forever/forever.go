d44c445c8e17009795115013b9e6ceda68c046b8
koyeo <koyeo@qq.com>
2023-01-11 15:32:05 +0800

    update forever

--- forever/forever.go
+++ forever/forever.go
@@ -1,17 +1,14 @@
 package forever
 
 import (
-	"context"
-	"fmt"
 	"time"
 )
 
 // Run 执行永久任务，类似 For 循环
 // 当 runner 返回时 error 时，将会退出循环
-func Run(ctx context.Context, interval time.Duration, runner func(ctx context.Context) error) (err error) {
+func Run(interval time.Duration, runner func()) {
 	if interval < time.Second {
-		err = fmt.Errorf("interval expect greater than 1 second")
-		return
+		interval = time.Second
 	}
 	timer := time.NewTimer(interval)
 	defer func() {
@@ -20,13 +17,8 @@ func Run(ctx context.Context, interval time.Duration, runner func(ctx context.Co
 	for {
 		select {
 		case <-timer.C:
-			err = runner(ctx)
-			if err != nil {
-				return
-			}
+			runner()
 			timer.Reset(interval)
-		case <-ctx.Done():
-			return
 		}
 	}
 }
--- forever/forever_test.go
+++ forever/forever_test.go
@@ -1,37 +1,12 @@
 package forever
 
 import (
-	"context"
-	"fmt"
-	"github.com/gozelle/testify/require"
 	"testing"
 	"time"
 )
 
 func TestForever(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer func() {
-		cancel()
-	}()
-	err := Run(ctx, time.Second, func(ctx context.Context) error {
+	Run(time.Second, func() {
 		t.Log(time.Now())
-		return nil
 	})
-	require.NoError(t, err)
-}
-
-func TestForeverError(t *testing.T) {
-	now := time.Now()
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer func() {
-		cancel()
-	}()
-	err := Run(ctx, time.Second, func(ctx context.Context) error {
-		t.Log(time.Now())
-		if time.Since(now) > 2*time.Second {
-			return fmt.Errorf("timeout")
-		}
-		return nil
-	})
-	require.Error(t, err)
 }
