11588f6d1580194f652f6ee55ccc8ee4671bbbd6
koyeo <guyue.me@gmail.com>
2023-06-07 23:43:21 +0800

    add batch

---
+++ batch/batch.go
@@ -0,0 +1,81 @@
+package batch
+
+import (
+	"sync"
+	"time"
+)
+
+type Handler[T any] func(done <-chan struct{}, data []T)
+
+func NewBatch[T any](interval time.Duration, threshold uint, handler Handler[T]) *Batch[T] {
+	return &Batch[T]{
+		data:      make([]T, 0),
+		interval:  interval,
+		handler:   handler,
+		done:      make(chan struct{}),
+		threshold: threshold,
+	}
+}
+
+type Batch[T any] struct {
+	lock      sync.Mutex
+	data      []T           // 存放数据的 slice
+	interval  time.Duration // 处理时间间隔
+	handler   Handler[T]
+	done      chan struct{}
+	threshold uint
+}
+
+func (b *Batch[T]) Stop() {
+	b.done <- struct{}{}
+}
+
+func (b *Batch[T]) Add(data T) {
+	b.lock.Lock()
+	defer func() {
+		b.lock.Unlock()
+	}()
+
+	b.data = append(b.data, data)
+}
+
+func (b *Batch[T]) Start() {
+
+	defer func() {
+		close(b.done)
+	}()
+
+	ticker := time.NewTicker(b.interval)
+	defer func() {
+		ticker.Stop()
+	}()
+
+	for {
+		select {
+		case <-b.done:
+			break
+		case <-ticker.C:
+			if len(b.data) > 0 {
+				b.process()
+			}
+		default:
+			if uint(len(b.data)) >= b.threshold {
+				b.process()
+			}
+		}
+	}
+}
+
+func (b *Batch[T]) process() {
+
+	b.lock.Lock()
+	defer func() {
+		b.lock.Unlock()
+	}()
+
+	if b.handler != nil {
+		b.handler(b.done, append([]T{}, b.data...))
+	}
+
+	b.data = nil
+}
---
+++ batch/batch_test.go
@@ -0,0 +1,30 @@
+package batch_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/gozelle/async/batch"
+)
+
+func TestBatch(t *testing.T) {
+
+	b := batch.NewBatch[int](2*time.Second, 500, func(done <-chan struct{}, data []int) {
+		t.Log(len(data))
+	})
+
+	go b.Start()
+
+	i := 0
+	for {
+		i++
+		if i > 100000000 {
+			break
+		}
+		b.Add(i)
+		//time.Sleep(1 * time.Millisecond)
+		//if i%100 == 0 {
+		//	time.Sleep(2 * time.Second)
+		//}
+	}
+}
