63260a6ecb38d89304af9e6f367498c380f81930
koyeo <koyeo@qq.com>
2023-08-09 13:54:57 +0800

    add sort

---
+++ slices/sort.go
@@ -0,0 +1,53 @@
+package slices
+
+import (
+	"github.com/gozelle/async/generics"
+	"sort"
+)
+
+var _ sort.Interface = (*sorter[any])(nil)
+
+type sorter[T any] struct {
+	list []T
+	less func(a, b T) bool
+}
+
+func (s sorter[T]) Len() int {
+	return len(s.list)
+}
+
+func (s sorter[T]) Less(i, j int) bool {
+	return s.less(s.list[i], s.list[j])
+}
+
+func (s sorter[T]) Swap(i, j int) {
+	s.list[i], s.list[j] = s.list[j], s.list[i]
+}
+
+func Sort[T any](items []T, less func(a, b T) bool) {
+	s := sorter[T]{
+		list: items,
+		less: less,
+	}
+	sort.Sort(s)
+}
+
+func SortAsc[T any, K generics.Ordered](items []T, key func(item T) K) {
+	s := &sorter[T]{
+		list: items,
+		less: func(a, b T) bool {
+			return key(a) < key(b)
+		},
+	}
+	sort.Sort(s)
+}
+
+func SortDesc[T any, K generics.Ordered](items []T, key func(item T) K) {
+	s := &sorter[T]{
+		list: items,
+		less: func(a, b T) bool {
+			return key(a) > key(b)
+		},
+	}
+	sort.Sort(s)
+}
---
+++ slices/sort_test.go
@@ -0,0 +1,45 @@
+package slices
+
+import "testing"
+
+type User struct {
+	Id   int
+	Name string
+}
+
+func TestSort(t *testing.T) {
+	list := []int{1, 3, 4, 2, 5}
+	strs := []string{"a", "c", "d", "e", "b"}
+	
+	Sort(list, func(a, b int) bool {
+		return a > b
+	})
+	t.Log(list)
+	
+	Sort(strs, func(a, b string) bool {
+		return a < b
+	})
+	t.Log(strs)
+}
+
+func TestSortAsc(t *testing.T) {
+	list := []int{1, 3, 4, 2, 5}
+	SortAsc(list, func(item int) int {
+		return item
+	})
+	t.Log(list)
+	
+	users := []User{
+		{Id: 1, Name: "tom"},
+		{Id: 2, Name: "jack"},
+		{Id: 3, Name: "jack"},
+	}
+	SortAsc(users, func(item User) int {
+		return item.Id
+	})
+	t.Log(users)
+	SortDesc(users, func(item User) int {
+		return item.Id
+	})
+	t.Log(users)
+}
